3c8593a07973ad895d5406180c0d8035fc78c9f7
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-04-01 08:12:02 +0300

    add a function that gets the user choice & add some missing comments from before

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3 ) + 1
+    let computerChoice = Math.floor(Math.random() * 3 ) + 1 //get a random number from 1-3
     
     switch (computerChoice) {
         case 1:
@@ -11,5 +11,22 @@ function getComputerChoice() {
         case 3:
             return "scissors";
             break;
-    }
+    }//to let the computer choose rock paper or scissors based on the random number 
+}
+
+function playerChoice() {
+    const userInput = prompt("Enter 1 for rock, 2 for paper and 3 for scissors");
+        //ask user for an input 
+    switch (+userInput) {// the "+" is to change the input from a string to an integer
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+    }//to match the number the player chose with rock paper or scissors
+
 }
