1b3719146eaebb4685061ab986bfdba2d5d4fcf8
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-04-29 16:29:06 +0300

    Clean up the code

--- javascript.js
+++ javascript.js
@@ -14,30 +14,10 @@ function getComputerChoice() {
     }//to let the computer choose rock paper or scissors based on the random number 
 }
 
-// function playerChoice() {
-//     const userInput = prompt("Enter 1 for rock, 2 for paper and 3 for scissors");
-//         //ask user for an input 
-//     switch (+userInput) {// the "+" is to change the input from a string to an integer
-//         case 1:
-//             return "rock";
-//             break;
-//         case 2:
-//             return "paper";
-//             break;
-//         case 3:
-//             return "scissors";
-//             break;
-//     }//to match the number the player chose with rock paper or scissors
-
-// }
-
 
 function playRound() {
     const computer = getComputerChoice();
     const player = playerChoice;
-
-    console.log(`You chose ${player} and the computer chose ${computer}`)
-    // to log the computer and the player's choice
 
     let Winner;
     let Tie;
@@ -46,47 +26,19 @@ function playRound() {
         alert("It's a tie, try again")
         return Tie = "tie"
     }else if(computer == "rock" && player == "scissors"){
-        // console.log("Computer wins")
         return Winner = "computer"
     }else if(computer == "paper" && player == "rock"){
-        // console.log("Computer wins")
         return Winner = "computer"
     }else if(computer == "scissors" && player == "paper"){
-        // console.log("Computer wins")
         return Winner = "computer"
     }else if(player == "rock" && computer == "scissors"){
-        // console.log("player wins")
         return Winner = "player"
     }else if(player == "paper" && computer == "rock"){
-        // console.log("player wins")
         return Winner = "player"
     }else if(player == "scissors" && computer == "paper"){
-        // console.log("player wins")
         return Winner = "player"
     }//see who wins at the game by evaluating the computer and the player choices
 }
-
-// function playGame() {
-//     let playerCounter = 0;
-//     let computerCounter = 0;
-//     // to keep count of how many wins
-
-//     for (let game = 0; game < 3; game++) {// play 3 rounds 
-//         let round = playRound(); // start the round 
-//         if (round == "tie" ){ //evaluate who wins
-//             console.log("it's a tie")
-//         }else if (round == "player"){
-//             playerCounter++ // add 1 to the counter if player wins
-//         }else if (round = "counter"){
-//             computerCounter++ // add 1 to thr counter if computer wins
-//         }
-//     }
-
-//     return `computer score ${computerCounter}, player score ${playerCounter}`;
-//     // returns scores
-// }   
-
-
 
 let playerChoice;
 function getPlayerChoice(){
